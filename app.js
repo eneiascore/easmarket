321fa7f0fcb5fd723ffe5a9c66844890b886dc3f
eneiascore <eneiascore@gmail.com>
2026-07-08 01:13:39 -0300

    implementa busca por nome e validação de duplicatas no cadastro

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-const { lerJogadores, salvarJogadores, adicionarJogador } = require('./services/mercado')
+const { lerJogadores, salvarJogadores, adicionarJogador, buscarJogadorPorNome} = require('./services/mercado')
 const Jogador = require('./models/Jogador')
 
 
 
 const jogadores = lerJogadores()
-console.log(jogadores)
 
+
+// teste de validação de duplicata
 // const Lamine = new Jogador("Lamine Yamal", 19, "Espanha", "barcelona", "PD", 89, 94)
 // adicionarJogador(Lamine)
+
+// teste de busca
+const jogadorEncontrado = buscarJogadorPorNome("Lamine Yamal")
+console.log(jogadorEncontrado)
--- services/mercado.js
+++ services/mercado.js
@@ -10,15 +10,34 @@ function salvarJogadores(jogadores) {
     fs.writeFileSync(caminhoArquivo, JSON.stringify(jogadores, null, 2))
 }
 
+function buscarJogadorPorNome(nome) {
+    const jogadores = lerJogadores()
+    return jogadores.find(jogador => jogador.Nome === nome)
+}
+
 function adicionarJogador(jogador) {
     const jogadores = lerJogadores()
+
+    // adicionando validação e eliminação de duplicatas na função.
+    const jogadorExistente = jogadores.find(j => j.Nome === jogador.Nome)
+
+    if (jogadorExistente) {
+        console.log("Não é possível adicionar um jogador que já está cadastrado. ");
+        return
+    }
+
     jogadores.push(jogador)
     salvarJogadores(jogadores)
+    console.log("jogador adicionado com sucesso! ");
+    
+    
 }
 
 module.exports = {
     lerJogadores,
     salvarJogadores,
-    adicionarJogador
+    adicionarJogador,
+    buscarJogadorPorNome
+
 }
 
